93404677165c606d62243dfb4f68e6eee35394bd
Dilsin Amirtharajah <KillerzDaggers@Outlook.com>
2022-10-08 12:37:04 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function greeting () {
+    console.log("Hello my name is Rashford");
+}
+
+greeting();
+greeting();
+
+const greeting1 = function() {
+    console.log("Hello my name is Dalot");
+}
+
+greeting1();
+
+const greeting2 = () => {
+    console.log("Hello my name is Malacia");
+}
+
+greeting2();
